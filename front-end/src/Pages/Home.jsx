47b8dcd93bf2046fb98165bc07a58b085660075b
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2024-04-18 14:05:45 +0530

    navbar and carousell added

---
+++ front-end/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CaptionCarousel from '../Components/CaptionCarousel'
+
+function Home() {
+  return (
+    <>
+    <div style={{width:'80%'}}>
+      <CaptionCarousel/>
+    </div>
+    </>
+  )
+}
+
+export default Home
