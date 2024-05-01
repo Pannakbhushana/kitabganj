9ccf52feeea155e5b7910413f376c234cab688a2
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2024-05-01 16:07:33 +0530

    private route added

--- front-end/src/Pages/AboutMe.jsx
+++ front-end/src/Pages/AboutMe.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box,Text } from '@chakra-ui/react'
 import React, { useContext, useEffect, useState } from 'react'
 import ProductDetails from '../PageComponent/ProductDetails'
 import HomeFeature from '../PageComponent/HomeFeature'
@@ -31,7 +31,10 @@ export default function AboutMe() {
     return <Loading/>
   }
   return (
-    <Box position={'relative'} minHeight={'600px'} width={'full'} overflow={'hidden'} paddingTop={'80px'}>
+    <Box position={'relative'} minHeight={'600px'} width={'full'} overflow={'hidden'} paddingTop={'80px'} >
+      <Box display="flex" justifyContent="start" ml='2%' mt={'1%'}>
+        <Text fontSize="2xl" as="b">परिचय : </Text>
+      </Box>
         {aboutMeContent && 
           aboutMeContent.map((content,index)=>{
            return <Box key={index}>
---
+++ front-end/src/SinglePage/SingleBlogPage.jsx
@@ -0,0 +1,93 @@
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+  } from '@chakra-ui/react'
+  import { useParams} from 'react-router-dom';
+  import { useContext, useEffect, useState } from 'react';
+  import { RenderContext } from '../ContextApi/RenderContext';
+import Loading from '../PageComponent/Loading';
+  
+  export default function SingleBlogPage() {
+    const [data, setData]=useState({});
+    const { isLoading,showLoading, hideLoading } = useContext(RenderContext);
+    const {image,description,heading}=data;
+    const param=useParams();
+
+    useEffect(()=>{
+        getData()
+      },[])
+    
+      const getData=()=>{
+        showLoading()
+        fetch(`http://localhost:8080/blog?_id=${param.id}`)
+          .then((res)=>res.json())
+          .then((res)=>{
+            hideLoading()
+            setData(res[0])
+          }) 
+          .catch((err)=>{
+            hideLoading()
+            console.log(err)
+          })
+      }
+
+      if(isLoading){
+        return <Loading/>
+      }
+
+    return (
+      <Container maxW={'7xl'} paddingTop={'80px'} minHeight={'650px'}>
+
+       <Box mt={'4%'} >
+        <Text fontSize="2xl" as={'b'}>{heading}</Text>
+       </Box>
+
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          mt={{ base: 0, md:"-3%", lg: '-3%' }}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 18, md: 24 }}>
+             <Flex>
+                <Image
+                rounded={'md'}
+                alt={'product image'}
+                src={image}
+                fit={'cover'}
+                align={'center'}
+                w={'100%'}
+                maxH={'500px'}
+                />
+          </Flex>
+
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={'column'}
+              divider={
+                <StackDivider borderColor='gray.200' />
+              }>
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                {description &&
+                  <Text
+                  color='gray.500'
+                  fontSize={'2xl'}
+                  fontWeight={'300'}>
+                  {description}
+                </Text>
+                }
+              </VStack>
+            </Stack>
+  
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    )
+  }
